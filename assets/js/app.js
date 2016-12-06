48faf2a63c5b5f6b03f31d32397e54d52c7b3d31
OKchesterOK <igor.ivanov.89@gmail.com>
2016-12-06 19:10:20 +0200

    scrollin pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,15 @@
     $('.logo-triangle').delay(4000).show(0);
 })();
 
+$(document).ready(function() {
+    $('a[href^="#"]').click(function(event){
+        var id_clicked_element = $(this).attr('href');
+        var destination = $(id_clicked_element).offset().top;
+        $('html, body').animate({scrollTop: destination}, 1000);
+        return false;
+    });
+});
+
 function showGoogleMaps() {
     var mapOptions = {
         zoom: 13,
