02c84dc7fd758e97c4dc235a881df7654dcf6de5
OKchesterOK <igor.ivanov.89@gmail.com>
2016-12-07 15:31:34 +0200

    some changes on contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $('a[href^="#"]').click(function(event){
         var id_clicked_element = $(this).attr('href');
         var destination = $(id_clicked_element).offset().top;
-        $('html, body').animate({scrollTop: destination}, 1000);
+        $('html, body').stop(true, false).animate({scrollTop: destination}, 600);
         return false;
     });
 });
